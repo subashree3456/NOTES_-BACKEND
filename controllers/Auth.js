7709f2a436249a49cc2dc5cde8a34bf297d54270
subashree3456 <subashree3003@gmail.com>
2024-11-28 10:30:46 +0530

    comit msggffjj

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -49,7 +49,7 @@ const Login=async(req,res)=>{
            res.cookie("token",token,{
             HttpOnly : true,
             secure:true, // turn it when you're in deployment
-            SameSite : false, // turn it into false when you're in production(deployment)
+            SameSite : None, // turn it into false when you're in production(deployment)
             maxAge:3 * 24 * 3600 * 1000
            })
            res.status(200).json({success:true,message:"user login successfully",user:FindUser,token})
