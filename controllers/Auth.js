b1d76e6b287253921b7bef0f22d3487f1cbcab74
subashree3456 <subashree3003@gmail.com>
2024-12-02 14:21:48 +0530

    comit msgkkll

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -49,7 +49,7 @@ const Login=async(req,res)=>{
            res.cookie("token",token,{
             HttpOnly : true,
             Secure:true,
-            SameSite:None,
+            SameSite:"None",
             maxAge:3 * 24 * 3600 * 1000
            })
            res.status(200).json({success:true,message:"user login successfully",user:FindUser,token})
